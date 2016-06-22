a938d401780839523d07cd477377af189a455d23
P Joe <Joe.Pen@joepen.local>
2016-06-22 14:36:39 +0800

    Initial Commit

---
+++ ParallelWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParallelWorld
+//
+//  Created by Joe.Pen on 6/22/16.
+//  Copyright © 2016 Joe.Pen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ParallelWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParallelWorld
+//
+//  Created by Joe.Pen on 6/22/16.
+//  Copyright © 2016 Joe.Pen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
